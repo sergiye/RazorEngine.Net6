970476309b6db972f2bc25f60ca894bb1f4fcbdf
Connie Yau <conniey@microsoft.com>
2017-07-06 12:52:22 -0700

    Add Test.RazorEngine.NUnitRunner to run tests against .NETCoreApp2.0

---
+++ src/test/Test.RazorEngine.NUnitRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using NUnitLite;
+
+namespace Test.RazorEngine.NUnitRunner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return new AutoRun(typeof(RazorEngineServiceTestFixture).Assembly).Execute(args);
+        }
+    }
+}
