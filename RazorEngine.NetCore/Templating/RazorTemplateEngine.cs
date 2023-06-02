cfdc87bce4f7b2b547d495c3e8542a51f1335dfd
Sergiy Egoshyn <sergeye@devart.com>
2023-06-02 17:48:14 +0300

    converted to ONLY support net6.0

--- RazorEngine.NetCore/Templating/RazorTemplateEngine.cs
+++ RazorEngine.NetCore/Templating/RazorTemplateEngine.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-#if NETCOREAPP3_1
 namespace Microsoft.AspNetCore.Razor.Language
 {
     using System;
@@ -222,4 +221,3 @@ namespace Microsoft.AspNetCore.Razor.Language
         }
     }
 }
-#endif
